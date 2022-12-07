16dd0952272c67ad670c714f72523c24b3db3a5d
KvotheKS <69768657+KvotheKS@users.noreply.github.com>
2022-12-07 13:29:02 -0300

    update from uni

--- main.cpp
+++ main.cpp
@@ -13,8 +13,8 @@ void read_asm(std::vector<std::vector<std::string>>& tokens, const char* file)
     int i = 0;
     for(eof = std::getline(fd, line).eof();;eof = std::getline(fd, line).eof(), i++)
     {
-        auto tokenized = tokenize(line, sep, spec);
-        tokens.push_back(tokenized);
+        tokenize(tokens, line, sep, spec);
+        // tokens.push_back(tokenized);
         if(eof) break;
     }    
 }
@@ -23,6 +23,13 @@ enum label_type
 {
     SPACE, CONST, EQU, INST, ERROR
 };
+
+struct macro_info
+{
+    int l, r; // l eh o indice onde o nome da macro foi definida e r onde ENDMACRO foi definido
+    std::vector<std::string> mapped;
+    std::map<int, std::vector<std::string>> tokens
+}
 
 void pre_proccess(std::vector<std::vector<std::string>>& tokens)
 {
@@ -143,12 +150,102 @@ void pre_proccess(std::vector<std::vector<std::string>>& tokens)
             }    
         }
     }
+    if(!sec_text)
+        err += "SECTION TEXT not declared (semantic error)\n";
+    
     // for(auto& it : equ)
     //     std::cout << it.first << ' ' << it.second << std::endl;
     if(err.size())
     {
         std::cout << "Pre-Processing dropped due to errors:\n\n" << err << std::endl;
         exit(EXIT_FAILURE);
+    }
+}
+
+void macro_processing(std::vector<std::vector<std::string>>& tokens, std::map<std::string, macro_info>& macros)
+{
+    std::string err;
+    bool sec_text = false, decl = false, sec_data = false;
+    int i = -1;
+    for(auto& line : tokens)
+    {
+        i++;
+        if(line.empty()) continue;
+        if(line.size() == 2 && line[0] == "SECTION" )
+        {
+            if(line[1] == "TEXT")
+                sec_text = true;
+            else if(line[1] == "DATA")
+                sec_data = true;
+        }
+        if(line.size() >= 2)
+        {
+            if(line[1] == "MACRO")
+            {
+                if(!sec_text || sec_data)
+                {
+                    err += LineLabel(i) + "MACROS have to be defined inside of SECTION TEXT(semantic error)\n";
+                    continue;
+                }
+
+                if(!label_valid(line[0]))
+                {
+                    err += LineLabel(i) + "MACRO keyword has to be identified by a token that follows the rule: ([A-Z_]+[A-Z0-9_]*:) (lexical error)\n";
+                    continue;
+                }
+
+                std::string tmp_lbl = line[0].substr(0, line[0].size()-1);
+                auto it = macros.find(tmp_lbl);
+
+                if(it != macros.end())
+                {
+                    err += LineLabel(i) + "MACRO with same id has already been defined (semantic error)\n";
+                    continue;
+                }
+
+                if(reserved.find(tmp_lbl) != reserved.end())
+                {
+                    err += LineLabel(i) + "Labels can't use reserved tokens (lexical error)\n";
+                    continue;
+                }
+
+                decl = true;
+                it = macros.insert({tmp_lbl, {i, -1, {}, {}}});
+                bool inval_arg = false;
+                for(int z = 2; z < line.size(); z++)
+                {
+                    if(line[z] == ",") continue;
+
+                    if(!macro_arg(line[z]))
+                    {inval_arg = true; break;}
+
+                    it->mapped.push_back(line[z].substr(0, line[z].size() - (line[z][line.size()-1] == ','))); // Se a gente recebe &arg, devemos retirar ','. 
+                }
+                if(inval_arg)
+                {
+                    err += LineLabel(i) + "Invalid argument format in MACRO declaration (lexical error)\n";
+                    continue;
+                }
+            }
+        }
+        if(line[0] == "ENDMACRO")
+        {
+            if(!decl)
+            {
+                err += LineLabel(i) + "Unexpected ENDMACRO label has been found (semantic error)\n";
+                continue;
+            }
+            macros[macros.size()-1].r = i;
+            decl=false;
+        }
+        if((auto it = macros.find(line[0])) != macros.end())
+        {
+            if(it->r == -1)
+            {
+                err += LineLabel(i) + "Can't call a macro inside itself (semantic error)\n";
+                continue;
+            }
+        }
     }
 }
 
@@ -169,7 +266,8 @@ void pre_proccess_file(std::vector<std::vector<std::string>>& tokens)
 int main()
 {
     std::vector<std::vector<std::string>> tokens;
-    
+    std::map<std::string, macro_info> macros;
+
     read_asm(tokens, "./bin.asm");
     upper_case(tokens);
     pre_proccess(tokens);
