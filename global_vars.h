746778200f5584ca9003963b63d2f89186d3a099
KvotheKS <69768657+KvotheKS@users.noreply.github.com>
2022-12-07 13:29:32 -0300

    macro and endmacro added

--- global_vars.h
+++ global_vars.h
@@ -6,5 +6,5 @@
 struct instr_info
 {};
 
-const std::set<std::string> reserved({"SPACE", "CONST", "IF", "EQU", "ADD", "MULT", "SUB", "DIV", "INPUT", "OUTPUT", "STOP", "COPY", "LOAD", "STORE", "JMPP", "JMPN", "JMP", "JMPZ"});
+const std::set<std::string> reserved({"SPACE", "CONST", "IF", "EQU", "ADD", "MULT", "SUB", "DIV", "INPUT", "OUTPUT", "STOP", "COPY", "LOAD", "STORE", "JMPP", "JMPN", "JMP", "JMPZ", "MACRO", "ENDMACRO"});
 const std::map<std::string, instr_info> instrs;
